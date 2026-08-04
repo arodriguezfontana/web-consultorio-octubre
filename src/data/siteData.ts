fdaffe8862dccf469b06dca8eef44eea11ed4a3e
Abril <abrilrodriguezfontana03@gmail.com>
2026-08-03 22:42:39 -0300

    style fixes

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -8,8 +8,7 @@ export const siteData = {
     phoneDisplay: "+54 9 11 1234-5678",
     whatsappUrl: "https://wa.me/5491112345678?text=Hola!%20Quisiera%20consultar%20por%20un%20turno%20en%20Octubre.",
     email: "contacto@consultoriooctubre.com",
-    address: "Calle Ejemplo 1234, Piso 1, Dpto A",
-    city: "Quilmes, Buenos Aires",
+    address: "Sarmiento 33, Quilmes",
     hours: "Lun a Vie: 08:00 - 20:00 hs",
     modality: "Atención presencial y virtual"
   },
