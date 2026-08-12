96940d4b4db7acd82b293988098acac25b20813c
Abril <abrilrodriguezfontana03@gmail.com>
2026-08-11 22:26:19 -0300

    galeria y fixes

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -10,7 +10,8 @@ export const siteData = {
     email: "contacto@consultoriooctubre.com",
     address: "Constitución 25, Bernal",
     hours: "Lun a Vie: 08:00 - 20:00 hs",
-    modality: "Atención presencial y virtual"
+    modality: "Atención presencial y virtual",
+    mapUrl: "https://www.google.com/maps/place/Espacio+Octubre/@-34.7067388,-58.2875857,810m/data=!3m1!1e3!4m15!1m8!3m7!1s0x95a33200debf62fb:0xce566cc36da33be3!2sConstituci%C3%B3n+25,+B1876AKF+Bernal,+Provincia+de+Buenos+Aires!3b1!8m2!3d-34.7067388!4d-58.2850108!16s%2Fg%2F11zdjkyq1_!3m5!1s0x95a333d2d8f1d3c7:0xe0e959e5997d7c3b!8m2!3d-34.7067388!4d-58.2850108!16s%2Fg%2F11zd8497gq?entry=ttu&g_ep=EgoyMDI2MDgwOS4wIKXMDSoASAFQAw%3D%3Dhttps://www.google.com/maps/place/Constituci%C3%B3n+25,+B1876AKF+Bernal,+Provincia+de+Buenos+Aires/@-34.7065449,-58.2859347,236m/data=!3m1!1e3!4m6!3m5!1s0x95a33200debf62fb:0xce566cc36da33be3!8m2!3d-34.7067388!4d-58.2850108!16s%2Fg%2F11zdjkyq1_?entry=ttu&g_ep=EgoyMDI2MDgwOS4wIKXMDSoASAFQAw%3D%3D"
   },
 
   social: {
